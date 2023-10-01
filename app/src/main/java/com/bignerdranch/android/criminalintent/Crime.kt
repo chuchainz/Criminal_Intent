733b537cbe198c3c51de656e1eeb083b5aaf8018
Jimmy Chu <chuchainz@outlook.com>
2023-10-01 16:21:44 -0700

    Criminal Intent: CH 12 Coroutines and Databases - Storing app data to a database

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -1,18 +1,23 @@
 package com.bignerdranch.android.criminalintent
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.Date
 import java.util.UUID
 
 //book sucks, idk why it tells me to do this when it doesnt work
-//class Crime {
-//    val id: UUID,
-//    val title: String,
-//    val date: Date,
-//    val isSolved: Boolean
-//}
+@Entity
 data class Crime(
-    val id: UUID = UUID.randomUUID(),
-    var title: String = "",
-    var date: Date = Date(),
-    var isSolved: Boolean = false
+    @PrimaryKey val id: UUID,
+    val title: String,
+    val date: Date,
+    val isSolved: Boolean
 )
+
+//@Entity
+//data class Crime(
+//    @PrimaryKey val id: UUID = UUID.randomUUID(),
+//    var title: String = "",
+//    var date: Date = Date(),
+//    var isSolved: Boolean = false
+//)
---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CriminalIntentApplication.kt
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.criminalintent
+
+import android.app.Application
+class CriminalIntentApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        CrimeRepository.initialize(this)
+    }
+}
